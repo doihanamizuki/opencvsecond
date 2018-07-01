59377d751141da6b71552f6a2dcada8a9ba794b4
doihanamizuki <40346568+doihanamizuki@users.noreply.github.com>
2018-07-02 05:54:38 +0900

    Update colorchange.py

--- colorchange.py
+++ colorchange.py
@@ -4,11 +4,11 @@ import cv2
 def myfunc(i):
     pass # do nothing
 
-cv2.namedWindow('contrast') # create win with win name
+cv2.namedWindow('colorchange') # create win with win name
 
-cv2.createTrackbar('red', 'contrast',  0, 100, myfunc) # callback func
-cv2.createTrackbar('green', 'contrast',  0, 100, myfunc) # callback func
-cv2.createTrackbar('blue', 'contrast',  0, 100, myfunc) # callback func
+cv2.createTrackbar('red', 'colorchange',  0, 100, myfunc) # callback func
+cv2.createTrackbar('green', 'colorchange',  0, 100, myfunc) # callback func
+cv2.createTrackbar('blue', 'colorchange',  0, 100, myfunc) # callback func
 
 
 cap = cv2.VideoCapture(0)
@@ -21,14 +21,14 @@ while(True):
     ret, frame = cap.read()
     if not ret: continue
     frame = frame/255
-    r = cv2.getTrackbarPos('red', 'contrast')*0.01# of the win
+    r = cv2.getTrackbarPos('red', 'colorchange')*0.01# of the win
     frame[:,:,2] *= r
-    g = cv2.getTrackbarPos('green', 'contrast')*0.01# of the win
+    g = cv2.getTrackbarPos('green', 'colorchange')*0.01# of the win
     frame[:,:,1 ]*= g
-    b = cv2.getTrackbarPos('blue', 'contrast')*0.01# of the win
+    b = cv2.getTrackbarPos('blue', 'colorchange')*0.01# of the win
     frame[:,:,0] *= b
 
-    cv2.imshow('contrast', frame)  # show in the win
+    cv2.imshow('colorchange', frame)  # show in the win
 
     k = cv2.waitKey(1)
     if k == ord('q') or k == 27:
